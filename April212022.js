561a49d016dc2de20281dd6adfb24d7fa68da991
Cecilia Martinez <cecimart3@gmail.com>
2022-04-21 17:19:04 +0100

    completed 3 problems

---
+++ April212022.js
@@ -0,0 +1,46 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  return (
+    name +
+    (name[0].toLowerCase() == "r" ? " plays" : " does not play") +
+    " banjo"
+  );
+}
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+function digitize(n) {
+  return n.toString().split("").reverse().map(Number);
+}
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros(array) {
+  let newArray = [];
+  let zeroes = [];
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (array[i] === 0) {
+      zeroes.push(0);
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray.concat(zeroes);
+}
